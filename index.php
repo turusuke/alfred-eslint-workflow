dc3e70b3f3d75437e6913efc2c8b0de855de14d0
turusuke <mail@turusuke.com>
2018-06-24 21:53:17 +0900

    [fix] get パラメータの値更新とAPIレスポンスの変更に伴い JSON 生成ロジックの修正

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
-$url = 'https://xwxg7mebsb-dsn.algolia.net/1/indexes/*/queries';
+$url = 'https://bh4d9od16a-dsn.algolia.net/1/indexes/*/queries';
 $account_data = array(
-    'x-algolia-api-key' => "653e00f423bee91f9863571eed16f2f5",
-    'x-algolia-application-id'=> "XWXG7MEBSB",
-    "x-algolia-agent"=>"Algolia%20for%20vanilla%20JavaScript%203.8.1"
+    'x-algolia-api-key' => "891b0e977d96c762a3821e0c00172ac9",
+    'x-algolia-application-id'=> "BH4D9OD16A",
+    "x-algolia-agent"=>"Algolia%20for%20vanilla%20JavaScript%20(lite)%203.24.7%3Bdocsearch.js%202.5.2"
 );
 
 $ch = curl_init();
@@ -15,20 +15,18 @@ curl_setopt($ch,CURLOPT_POST, true);
 curl_setopt($ch, CURLOPT_POSTFIELDS, '{"requests":[{"indexName":"eslint","params":"query='.$query.'&hitsPerPage=20"}]}');
 
 $res = json_decode(curl_exec($ch));
-$filterd = array_filter($res->results[0]->hits, function($v) {
-    return $v->category === 'Rules';
-});
+$filterd = $res->results[0]->hits;
 curl_close($ch);
 
 $obj = array('items' => array());
 
 foreach($filterd as $value) {
     $obj['items'][] = array(
-        "uid" => $value->title,
-        "title" => $value->title,
-        "subtitle" => $value->display_title,
-        "arg" => "https://eslint.org".$value->url,
-        "autocomplete" => $value->title,
+        "uid" => $value->anchor,
+        "title" => $value->hierarchy->lvl0,
+        "subtitle" => $value->hierarchy->lvl1,
+        "arg" => $value->url,
+        "autocomplete" => $value->anchor,
     );
 }
 
